f9ef58a58a9982c6ac38481bb36d6e4e67c003cd
back2childhood <1939371557@qq.com>
2024-08-06 16:42:54 -0700

    login/test form/collect data

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,5 +1,51 @@
+import "./index.scss"
+import { Card, Form, Input, Button } from "antd"
+import logo from '@/assets/logo.png'
+
 const Login = () => {
-    return <div>this is Login</div>
+    const onFinish = (value) => {
+        console.log(value)
+    }
+
+
+    return (
+        <div className="login">
+            <Card className="login-container">
+                <img className="login-logo" src={logo} alt="" />
+                <Form onFinish={onFinish} validateTrigger="onBlur">
+                    <Form.Item
+                        name="mobile"
+                        rules={[
+                            {
+                                required: true,
+                                message: 'please input your number!',
+                            },
+                            {
+                                pattern: /^[0-9]*$/,
+                                message: 'wrong number pattern!'
+                            }
+                        ]}>
+                        <Input size="large" placeholder="please input your number" />
+                    </Form.Item>
+                    <Form.Item
+                        name="code"
+                        rules={[
+                            {
+                                required: true,
+                                message: 'please input your security code!',
+                            },
+                        ]}>
+                        <Input size="large" placeholder="please input security code" />
+                    </Form.Item>
+                    <Form.Item>
+                        <Button type="primary" htmlType="submit" size="large" block>
+                            login
+                        </Button>
+                    </Form.Item>
+                </Form>
+            </Card>
+        </div>
+    )
 }
 
 export default Login
